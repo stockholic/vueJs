73cc2df60a97ad53131a1481541adbf780b011be
stockholic <parkjjss@gmail.com>
2020-01-29 10:04:51 +0900

    Create Home.vue

---
+++ view/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+  <h1>HOME</h1>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+/* import Home from "@/components/Home.vue";
+
+export default {
+  name: "home",
+  components: {
+    Home
+  }
+}; */
+</script>
