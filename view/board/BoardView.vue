d9cefb767389ca3a72edae198f9d4fd16e4dbabb
stockholic <parkjjss@gmail.com>
2020-01-29 10:13:30 +0900

    Create BoardView.vue

---
+++ view/board/BoardView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <div>
+        <h1>View</h1>
+        <p>params : {{ $route.params }}</p>
+    </div>
+  </v-container>
+</template>
+<script>
+export default {
+   computed: {
+        getParam(){
+            return this.$route.params
+        }
+    },
+    created() {
+        // eslint-disable-next-line no-console
+        // console.log("router", this.$router)
+        // eslint-disable-next-line no-console
+        // console.log("route", this.$route)
+    },
+}
+</script>
