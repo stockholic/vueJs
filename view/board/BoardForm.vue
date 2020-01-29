99b4950af367dfeeaad7f77e08d8564d6768e508
stockholic <parkjjss@gmail.com>
2020-01-29 10:13:46 +0900

    Create BoardForm.vue

---
+++ view/board/BoardForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <div>
+        <h1>Form</h1>
+        <p>params : {{ $route.params }}</p>
+        <p>ex) /xxx/1234 : {{ getParam }} seqNo :  {{ getParam.seqNo }}</p>
+
+        <hr>
+
+        <h1>Query</h1>
+        <p>query : {{ $route.query }} seqNo :  {{ $route.query.seqNo }}</p>
+        <p>ex) /xxx?seqNo=merong</p>
+    </div>
+  </v-container>
+</template>
+<script>
+export default {
+   computed: {
+        getParam(){
+            return this.$route.params
+        }
+    },
+    created() {
+        // eslint-disable-next-line no-console
+        console.log("router", this.$router)
+        // eslint-disable-next-line no-console
+        console.log("route", this.$route)
+    },
+}
+</script>
