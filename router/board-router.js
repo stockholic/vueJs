9ada8fd1b7c73dd1638582164e03e178f2cda4af
stockholic <parkjjss@gmail.com>
2020-01-30 13:54:09 +0900

    Update board-router.js

--- router/board-router.js
+++ router/board-router.js
@@ -3,4 +3,7 @@ export default [
   ,{path: "/board/list/:page", name: "board-list", component: () => import("../views/board/BoardList.vue")}
   ,{path: "/board/view/:seqNo", name: "board-view", component: () => import("../views/board/BoardView.vue")}
   ,{path: "/board/form/:seqNo", name: "board-form", component: () => import("../views/board/BoardForm.vue")}
+  
+  ,{path: "/board", name: "board", component: () => import("../views/board/Board.vue")}
+  
 ];
