ce4f6b65a52ce6fd14ed448a468b74d99c5244d4
stockholic <parkjjss@gmail.com>
2020-01-30 13:54:54 +0900

    Create board.vue

---
+++ view/board/board.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <component v-bind:is="selectedComponent" 
+      :page="page" 
+      :seqNo="seqNo"
+      @goView="view"
+      @goList="list"
+    >
+    </component>
+    
+  </div>
+</template>
+
+<script>
+import BoardList from "./BoardList"
+import BoardView from "./BoardView"
+
+  export default {
+		data: function() {
+			return {
+        selectedComponent: 'BoardList'
+        ,page : 1
+        ,seqNo : 0
+			}
+		},
+		components: {
+      BoardList,
+      BoardView
+    },
+    methods: {
+      view(seqNo){
+        this.selectedComponent = "BoardView";
+        this.seqNo = seqNo;
+      },
+      list(){
+        this.selectedComponent = "BoardList";
+      }
+    },
+  }
+</script>
