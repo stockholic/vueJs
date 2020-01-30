b065b421a915886d73ddc342b492d24bc55340da
stockholic <parkjjss@gmail.com>
2020-01-30 13:55:41 +0900

    Update BoardView.vue

--- view/board/BoardView.vue
+++ view/board/BoardView.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container>
     <div>
-        <h1>View</h1>
+        <h1>View {{ page }} {{ seqNo }}</h1>
         <p>params : {{ $route.params }}</p>
+        <p><button @click="goList()">List</button></p>
     </div>
   </v-container>
 </template>
 <script>
 export default {
+   props : ['page', 'seqNo'],
+
    computed: {
         getParam(){
             return this.$route.params
@@ -18,6 +21,11 @@ export default {
         // console.log("router", this.$router)
         // eslint-disable-next-line no-console
         // console.log("route", this.$route)
+    },
+    methods: {
+       goList(){
+          this.$emit('goList')
+        },
     },
 }
 </script>
